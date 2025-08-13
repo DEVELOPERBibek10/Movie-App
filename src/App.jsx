c36d3e295496e9c651d43f9fc73b0e075cbe9667
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2025-08-13 23:32:40 +0545

    add trending section in React Movie App

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,26 @@ import { useFetchMovie } from "./Hooks/useFetchMovie";
 import Loader from "./Components/Loader";
 import MovieCard from "./Components/MovieCard";
 import { useDebounce } from "./Hooks/useDebounce";
+import { useTrendingMovies } from "./Hooks/useTrendingMovies";
 
 function App() {
   const [searchText, setSearchText] = useState("");
   const { movieList, isLoading, errorMessage, fetchMovies } = useFetchMovie();
   const debouncedFunction = useDebounce(fetchMovies, 800);
+  const {
+    fetchTrendingMovies,
+    trendingMovies,
+    trendingLoading,
+    trendingError,
+  } = useTrendingMovies();
 
   useEffect(() => {
     debouncedFunction(searchText);
   }, [debouncedFunction, searchText]);
+
+  useEffect(() => {
+    fetchTrendingMovies();
+  }, []);
 
   return (
     <>
@@ -26,8 +37,29 @@ function App() {
             </h1>
             <Search searchText={searchText} setSearchText={setSearchText} />
           </header>
+          {trendingLoading ? (
+            <div className="w-[80vw] flex justify-center items-center">
+              <Loader />
+            </div>
+          ) : trendingError ? (
+            <p className="text-red-500 text-center">{errorMessage}</p>
+          ) : (
+            trendingMovies.length > 0 && (
+              <section className="trending">
+                <h2>Trending</h2>
+                <ul>
+                  {trendingMovies.map((movie, idx) => (
+                    <li key={movie.$id}>
+                      <p>{idx + 1}</p>
+                      <img src={movie.posterUrl} alt={movie.title} />
+                    </li>
+                  ))}
+                </ul>
+              </section>
+            )
+          )}
           <section className="all-movies">
-            <h2 className="mt-[40px]">All Movies</h2>
+            <h2 className="mt-[20px]">All Movies</h2>
 
             {isLoading ? (
               <div className="w-[80vw] flex justify-center items-center">
