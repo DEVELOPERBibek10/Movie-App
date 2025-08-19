8f13618839506491c14ff0397b8c66635344a81e
DEVELOPERBibek10 <bibekpyakurel7@gmail.com>
2025-08-19 22:17:56 +0545

    update constant file

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -3,8 +3,8 @@ const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTION = {
   method: "GET",
   headers: {
-    accept: "application/json",
     Authorization: `Bearer ${API_KEY}`,
+    accept: "application/json",
   },
 };
 
